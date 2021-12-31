f91c533b42f747bfb884c9d0bc2a80dfd7ecfc75
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2021-12-31 14:33:37 +0100

    upload proto

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -42,6 +42,21 @@ public class Test {
                 System.out.printf("%s (%s, %s, %s, %s)\n", file.getName(), file.getMimeType(), file.getId(), file.getModifiedTime(), result.getNextPageToken());
             }
         }
+        FileList specResult = service.files().list()
+                .setQ("name='test.txt' and '1lUN7HEFiDVNjPwYWT136XChDh5JDLoz_' in parents") // also coming from NiFi
+                .setPageSize(100)
+                .setFields("nextPageToken, files(id, name, mimeType, modifiedTime)")
+                .execute();
+        List<File> specFiles = specResult.getFiles();
+        if (specFiles == null || specFiles.isEmpty()) {
+            System.out.println("No files found (spec).");
+        } else {
+            System.out.println("Files (spec):");
+            for (File file : specFiles) {
+                System.out.printf("%s (%s, %s, %s, %s)\n", file.getName(), file.getMimeType(), file.getId(), file.getModifiedTime(), specResult.getNextPageToken());
+            }
+        }
+        System.out.println("Work one file...");
         final Drive.Files.Get get = service.files()
             .get("1Kv2RXrcQtwAmo9f66UbEJpEWvZAPGiWx")
             .setFields("id, name, mimeType, createdTime, modifiedTime");
@@ -55,21 +70,59 @@ public class Test {
             .setFields("owners");
         File folderMeta = folderGet.execute();
         //System.out.println(folderMeta.getOwners());
-        service.files().emptyTrash();
+        service.files().emptyTrash(); // (srv account has sep.)
         final About.StorageQuota quota = service.about().get().setFields("storageQuota(*)").execute().getStorageQuota();
-        System.out.println("Limit: " + quota.getLimit() + " / used: " + quota.getUsage());
-        System.exit(0);
+        System.out.println("Limit: " + quota.getLimit() + " - used: " + quota.getUsage());
+        //System.exit(0);
         // upload and overwrite if nec - srv account has sep. quota of 15GB (ideally, target should be shared drive, not shared personal folder!)
-        File fileMetadata = new File();
-        fileMetadata.setName("photo.jpg");
-        fileMetadata.setParents(Collections.singletonList("1lUN7HEFiDVNjPwYWT136XChDh5JDLoz_"));
-        //fileMetadata.setOwners(folderMeta.getOwners());
-        java.io.File filePath = new java.io.File("/Users/sebastianrothbucher/Desktop/photo.jpg");
-        FileContent mediaContent = new FileContent("image/jpeg", filePath);
-        File uploaded = service.files().create(fileMetadata, mediaContent)
-                .setFields("id")
-                .execute();
-        System.out.println("File ID: " + uploaded.getId());
-        // still todo: check name exists & overwrite + create folders when folder/filename format
+        final String[] targetName = "newsub/photo.jpg".split("/");
+        String currentFolderId = "1lUN7HEFiDVNjPwYWT136XChDh5JDLoz_";
+        for (int i = 0; i < targetName.length; i++) {
+            final boolean targetNameLast = (i == (targetName.length - 1));
+            // check for existing
+            String existId = null;
+            FileList existResult = service.files().list()
+                    .setQ("name='" + targetName[i] + "' and '" + currentFolderId + "' in parents")
+                    .setPageSize(1)
+                    .setFields("files(id, name)")
+                    .execute();
+            final List<File> existFiles = existResult.getFiles();
+            if (!(existFiles == null || existFiles.isEmpty())) {
+                existId = existFiles.get(0).getId();
+            }
+            if (!targetNameLast) { // folder (create or just move on)
+                if (null == existId) {
+                    File folderMetadata = new File();
+                    folderMetadata.setName(targetName[i]);
+                    folderMetadata.setMimeType("application/vnd.google-apps.folder");
+                    folderMetadata.setParents(Collections.singletonList(currentFolderId));
+                    File folder = service.files().create(folderMetadata)
+                            .setFields("id")
+                            .execute();
+                    System.out.println("Folder ID: " + folder.getId());
+                    currentFolderId = folder.getId();
+                } else {
+                    currentFolderId = existId;
+                }
+            } else { // the file itself (create or overwrite)
+                //fileMetadata.setOwners(folderMeta.getOwners());
+                java.io.File filePath = new java.io.File("/Users/sebastianrothbucher/Desktop/photo.jpg");
+                FileContent mediaContent = new FileContent("image/jpeg", filePath);
+                File uploaded = null;
+                if (null == existId) {
+                    File fileMetadata = new File();
+                    fileMetadata.setName(targetName[i]);
+                    fileMetadata.setParents(Collections.singletonList(currentFolderId));
+                    uploaded = service.files().create(fileMetadata, mediaContent)
+                            .setFields("id")
+                            .execute();
+                } else {
+                    uploaded = service.files().update(existId, null, mediaContent)
+                            .setFields("id")
+                            .execute();
+                }
+                System.out.println("File ID: " + uploaded.getId());
+            }
+        }
     }
 }
